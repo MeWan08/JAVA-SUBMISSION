5eff08da7da217537f48c1c3998b29bd0ae4d4d1
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-01-31 13:11:48 +0530

    Creating object of calculator class and displaying output

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,5 +1,9 @@
 // Main.java
 class Main{
 	public static void main(String[] args){
+		Calculator cal = new Calculator();
+		System.out.println("Sum is:" + cal.sum());
+		System.out.println("Difference is:" + cal.difference());
+		System.out.println("Product is:"+ cal.product());  
 	}
 }
