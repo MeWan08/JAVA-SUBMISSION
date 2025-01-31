b36ff5d8653e077d55dafbed156d252d6eff4695
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-01-31 13:08:29 +0530

    Difference function

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -7,4 +7,10 @@ class Calculator{
 		int sum = numbers[0] + numbers[1];
 		return sum;	
 	}
+	int difference(){
+		UserInput ui = new UserInput();
+		int[] numbers = ui.userInput();
+		int diff = numbers[1] - numbers[0];
+		return diff;
+	}
 }
