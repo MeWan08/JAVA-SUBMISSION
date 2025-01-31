5bff8eaac761e15c1bc3018ec77258b21f19ec5d
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-01-31 13:03:51 +0530

    OddEven function

--- ArrayFunctions/ArrayFunctions.java
+++ ArrayFunctions/ArrayFunctions.java
@@ -4,4 +4,22 @@ class ArrayFunctions{
 	void display(int[] array){
 		System.out.println("Array is:" + Arrays.toString(array));
 	}
+	void OddEven(int[] array){
+		int[] even = new int[5];
+		int[] odd = new int[5];
+		int odd_index =0;
+		int even_index = 0;
+		for(int i=0;i<array.length;i++){
+			if(array[i]%2==0){
+				even[even_index] = array[i];
+				even_index++;		
+			}
+			else{
+				odd[odd_index] = array[i];
+				odd_index++;	
+			}
+		}
+		display(even);
+		display(odd);
+	}
 }
