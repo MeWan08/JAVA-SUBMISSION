5a6e368fe97e30ac21352107b823df1f0c66eed8
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-01-31 12:38:22 +0530

    Scanner Object Creation

--- Main/Main.java
+++ Main/Main.java
@@ -5,5 +5,18 @@ import java.util.*;
 class Main{
 	public static void main(String[] args)
 	{
+		/*
+		//Input using scanner
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("Pleaase enter the first number:");
+		int first_num = sc.nextInt();
+		System.out.println("Please enter the second number:");
+		int second_num = sc.nextInt();
+		int result = first_num +second_num;
+		System.out.println("The sum is " +result);
+		*/
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
 	}
 }
