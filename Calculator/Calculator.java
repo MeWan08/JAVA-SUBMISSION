f2d6ddc195ea5122a91a84987e60376b9bd422ab
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-01-31 13:08:07 +0530

    Sum function

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,4 +1,10 @@
 // Calculator.java
 import java.util.*;
 class Calculator{
+	int sum(){
+		UserInput ui = new UserInput();
+		int[] numbers = ui.userInput();
+		int sum = numbers[0] + numbers[1];
+		return sum;	
+	}
 }
