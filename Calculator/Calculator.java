720e7ffc1d0c1f187c93b98369c346c0bf52ae20
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-01-31 13:08:43 +0530

    Product function

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -13,4 +13,10 @@ class Calculator{
 		int diff = numbers[1] - numbers[0];
 		return diff;
 	}
+	int product(){
+		UserInput ui = new UserInput();
+		int[] numbers = ui.userInput();
+		int product = numbers[0] * numbers[1];
+		return product;
+	}
 }
