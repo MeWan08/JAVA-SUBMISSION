6f5f852a20b489791278d7f72518d0495a2e1839
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-01-31 13:04:40 +0530

    OddEvenList function

--- ArrayFunctions/ArrayFunctions.java
+++ ArrayFunctions/ArrayFunctions.java
@@ -25,4 +25,21 @@ class ArrayFunctions{
 	void displayList(ArrayList<Integer> array){
 		System.out.println("Array is:" + array);
 	}
+	void OddEvenList(int[] array){
+		ArrayList<Integer> even = new ArrayList<Integer>();
+		ArrayList<Integer> odd = new ArrayList<Integer>();
+
+		for(int i=0;i<array.length;i++){
+			if(array[i]%2==0){
+				even.add(array[i]);
+			
+			}
+			else{
+				odd.add(array[i]);
+					
+			}
+		}
+		displayList(even);
+		displayList(odd);
+}
 }
