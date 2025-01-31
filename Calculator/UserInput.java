9cf8fba9ee7c5a4bc786d2b3c7c01e33574bf970
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-01-31 13:10:33 +0530

    UserInput as array

--- Calculator/UserInput.java
+++ Calculator/UserInput.java
@@ -1,4 +1,13 @@
 // UserInput.java
 import java.util.*; 
 class UserInput{
+	int[] userInput(){
+	int[] numbers = new int[2];
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the first number:");
+	numbers[0] = sc.nextInt();
+	System.out.println("Enter the second number:");
+	numbers[1] = sc.nextInt();
+	return numbers;
+	}
 }	
