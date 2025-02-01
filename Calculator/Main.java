1c9327105710be76f119bc19090bd734eeb711cd
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-02-01 18:48:40 +0530

    Taking user input for Operation

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -9,6 +9,19 @@ public class Main {
         
         int choice;
         do {
+            System.out.println("Select one of the following:");
+            System.out.println("1. Addition"); 
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Fibonacci Sequence");
+            System.out.println("6. Sum of Array");
+            System.out.println("7. Mean of Array");
+            System.out.println("8. Variance of Array");
+            System.out.println("9. Standard Deviation of Array");
+            System.out.println("10. Exit");
+            
+            choice = sc.nextInt();
         } while(choice != 10);
         sc.close();
     }
