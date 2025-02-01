9d19b72e4f03a30a673f9a17d186c3516f31d49a
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-02-01 18:47:01 +0530

    Object creation

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        Calculator cal = new Calculator();
+        Scanner sc = new Scanner(System.in);
+        UserInput ui = new UserInput();
+        
     }
 }
