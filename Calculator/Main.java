e20695098c63aa41b96f6697d4042fba01be1cd0
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-02-01 18:45:45 +0530

    Deletion in file

--- Calculator/Main.java
+++
@@ -1,9 +0,0 @@
-// Main.java
-class Main{
-	public static void main(String[] args){
-		Calculator cal = new Calculator();
-		System.out.println("Sum is:" + cal.sum());
-		System.out.println("Difference is:" + cal.difference());
-		System.out.println("Product is:"+ cal.product());  
-	}
-}
