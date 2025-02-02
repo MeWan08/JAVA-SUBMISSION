0583cfcba16cabe8c77f29be0f2cf74005d8eb47
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-02-02 12:45:08 +0530

    Update Main.java

--- ArrayFunctions/Main.java
+++ ArrayFunctions/Main.java
@@ -1,12 +1,9 @@
 //Main.java
-
+import java.util.*;
 class Main{
 	public static void main(String[] args){
 		ArrayInput ai = new ArrayInput();
 		ArrayFunctions af = new ArrayFunctions();
-		af.display(ai.userInput());
-		af.OddEven(ai.userInput());
-		af.OddEvenList(ai.userInput());
 	}
 	  
 }
