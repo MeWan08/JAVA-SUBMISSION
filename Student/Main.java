c7f8907ba51c8ca79894dea2413e5d235d094661
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-02-03 20:25:51 +0530

    Extra Student input

--- Student/Main.java
+++ Student/Main.java
@@ -27,5 +27,23 @@ class Main{
         System.out.println("Do you want to Continue(Yes/No) :  ");
         choice = scan.nextLine();
         }while(!choice.equalsIgnoreCase("No"));
+
+        System.out.println("Enter Name :  ");
+        String name = scan.nextLine();
+
+        System.out.println("Enter PRN :  ");
+        long prn = Long.parseLong(scan.nextLine());
+
+        System.out.println("Enter GPA :  ");
+        double gpa = Double.parseDouble(scan.nextLine());
+
+        System.out.println("Enter Batch :  ");
+        String batch = scan.nextLine();
+
+        System.out.println("Enter Branch :  ");
+        String branch = scan.nextLine();
+
+        Student student = new Student(name,prn,gpa,batch,branch);
+        operations.addStudents(student);
     }
 }
