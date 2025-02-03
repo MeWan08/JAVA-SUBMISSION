d054fbc7944315872352b267d66637d83a5a540e
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-02-03 16:10:57 +0530

    ArrayList Creation

--- Student/StudentOperations.java
+++ Student/StudentOperations.java
@@ -1,5 +1,6 @@
 //StuentOperations.java
 import java.util.*;
 class StudentOperations{
+	ArrayList<Student> students; //arraylist created with the class Student as the data type as student has multiple data types
 	
 }
