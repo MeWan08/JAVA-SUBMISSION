cd3c17a0d7570df49dec488e2ca1d9c10e43cb03
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-02-03 20:25:05 +0530

    Object creation

--- Student/Main.java
+++ Student/Main.java
@@ -2,5 +2,7 @@ import java.util.* ;
 
 class Main{
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations() ;
     }
 }
