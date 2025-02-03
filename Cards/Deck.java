891d8a518cb508ae5db83ad7727202b496870628
MeWan <157877376+MeWan08@users.noreply.github.com>
2025-02-03 20:20:36 +0530

    createDeck function
    Function to populate the deck

--- Cards/Deck.java
+++ Cards/Deck.java
@@ -6,4 +6,31 @@ class Deck{
 	public Deck(){
 		this.deck = new ArrayList<>();
 	} 
+	// to populate the deck
+	public void createDeck(){
+		String[] ranks = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+		String[] suitsBlack = {"Spades", "Clubs"};
+		String[] suitsRed = {"Hearts", "Diamonds"};
+		String[] colours = {"Red", "Black"};
+		
+		for(String colour:colours){
+			if(colour == "Red"){
+				for(String rank:ranks){
+					for(String suit:suitsRed){
+						deck.add(new Cards(rank, suit, colour));
+							
+					}
+				}
+			}
+			else{
+				for(String rank:ranks){
+					for(String suit:suitsBlack){
+						deck.add(new Cards(rank, suit, colour));
+						
+					}
+				}
+			}
+		}
+		System.out.println("The count of the deck is:" + deck.size());
+	}
 }
