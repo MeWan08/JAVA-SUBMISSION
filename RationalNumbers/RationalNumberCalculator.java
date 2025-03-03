c59fc0f685cf2736baf80814bdeb888318cd1c95
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-03 20:55:33 +0530

    Nested try and catch

--- RationalNumbers/RationalNumberCalculator.java
+++ RationalNumbers/RationalNumberCalculator.java
@@ -2,5 +2,12 @@ import java.util.*;
 public class RationalNumberCalculator {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+            try{
+                r1 = new RationalNumber(num, den);
+                r2 = new RationalNumber(num2, den2);
+            }
+            catch(ArithmeticException e){
+                System.out.println("Error while creating objects.");
+            }
     }
 }
