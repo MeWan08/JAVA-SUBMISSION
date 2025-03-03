af49312ddef9fab40a4ee264a8d089ede3892711
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-03 20:51:28 +0530

    Constructor creation

--- RationalNumbers/RationalNumber.java
+++ RationalNumbers/RationalNumber.java
@@ -3,4 +3,12 @@ import java.math.BigInteger;
 public class RationalNumber {
     private int numerator;
     private int denominator;
+    public RationalNumber(int numerator, int denominator){
+        if(denominator==0){
+            throw new ArithmeticException("Denominator can not divide the numerator");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+        simplify();
+    }
 }
