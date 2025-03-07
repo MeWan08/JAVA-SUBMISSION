26d18a15de8b2cffc4cef23b23cb8aec1a94dda5
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:46:48 +0530

    constructor

--- Shapes/EquilateralPyramid.java
+++ Shapes/EquilateralPyramid.java
@@ -2,4 +2,10 @@
 class EquilateralPyramid extends Shape implements Volume {
     private double baseEdge;
     private double height;
+
+    public EquilateralPyramid(double baseEdge, double height) {
+        super("Equilateral Pyramid (Square Base)");
+        this.baseEdge = baseEdge;
+        this.height = height;
+    }
 }
