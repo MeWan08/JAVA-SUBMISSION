e2306612ba49b548dce0a0728500f4750fbd14f4
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:50:24 +0530

    Non abstract method

--- Shapes/Shape.java
+++ Shapes/Shape.java
@@ -1,4 +1,9 @@
 // Abstract class representing a Shape
 abstract class Shape {
     protected String shapeName;
+
+    // Non-abstract method
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
 }
