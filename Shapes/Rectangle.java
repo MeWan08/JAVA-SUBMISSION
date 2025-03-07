5fe4f98b97e1cfea13ecf8fed9a38219cde1dbd4
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:49:33 +0530

    CalculatePerimeter function

--- Shapes/Rectangle.java
+++ Shapes/Rectangle.java
@@ -13,4 +13,9 @@ class Rectangle extends Shape {
     public double calculateArea() {
         return length * width;
     }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
 }
