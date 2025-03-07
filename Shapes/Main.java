f85f6900ec242a43404a9bab6a6e1a6a2d0b150e
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:38:11 +0530

    Scanner object close

--- Shapes/Main.java
+++ Shapes/Main.java
@@ -81,5 +81,7 @@ public class Main {
                     System.out.println("Invalid choice! Please try again.");
             }
         } while (choice != 7);
+
+        scanner.close();
     }
 }
