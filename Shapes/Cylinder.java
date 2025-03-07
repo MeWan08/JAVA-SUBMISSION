ad663d05d1c6f3a6a915885894f1405a23ad5f3f
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:44:24 +0530

    constructor

--- Shapes/Cylinder.java
+++ Shapes/Cylinder.java
@@ -3,4 +3,9 @@ class Cylinder extends Shape implements Volume {
     private double radius;
     private double height;
 
+    public Cylinder(double radius, double height) {
+        super("Cylinder");
+        this.radius = radius;
+        this.height = height;
+    }
 }
