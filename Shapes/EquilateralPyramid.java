1f342465bb3315f21444f89395efbd59e6dc9cd4
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:46:24 +0530

    Class members

--- Shapes/EquilateralPyramid.java
+++ Shapes/EquilateralPyramid.java
@@ -1,3 +1,5 @@
 // EquilateralPyramid class extending Shape and implementing Volume
 class EquilateralPyramid extends Shape implements Volume {
+    private double baseEdge;
+    private double height;
 }
