f38c35ef347bc2b6b2f122e0bcd8a1da0d4fbcd8
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:53:27 +0530

    constructor

--- Shapes/Square.java
+++ Shapes/Square.java
@@ -1,4 +1,9 @@
 // Square class extending Shape
 class Square extends Shape {
     private double side;
+
+    public Square(double side) {
+        super("Square");
+        this.side = side;
+    }
 }
