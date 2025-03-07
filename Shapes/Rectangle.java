08bae1625b8b5e69db162a13b6013022e817d364
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:49:00 +0530

    Constructor

--- Shapes/Rectangle.java
+++ Shapes/Rectangle.java
@@ -3,4 +3,9 @@ class Rectangle extends Shape {
     private double length;
     private double width;
 
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
 }
