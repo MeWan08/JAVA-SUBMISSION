0c5b68b85a273fb01ccefb40f737678e465f5aef
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-07 21:51:48 +0530

    constructors

--- Shapes/Sphere.java
+++ Shapes/Sphere.java
@@ -1,4 +1,10 @@
 // Sphere class extending Shape and implementing Volume
 class Sphere extends Shape implements Volume {
     private double radius;
+
+    public Sphere(double radius) {
+        super("Sphere");
+        this.radius = radius;
+    }
+
 }
