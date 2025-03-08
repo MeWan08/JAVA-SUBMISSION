ce1e629adf34d274989c74546ab17f5f64202f79
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-08 23:14:51 +0530

    display function

--- Duck/RedHeadDuck.java
+++ Duck/RedHeadDuck.java
@@ -4,4 +4,9 @@ public class RedHeadDuck extends Duck {
         flyBehavior = new Fly();
         swimBehavior = new Swim();
     }
+
+    @Override
+    public void display() {
+        System.out.println("I am a Redhead Duck.");
+    }
 }
