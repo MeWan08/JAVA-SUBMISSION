d94489322bd10013a115b6eef03962923a1dedb6
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-08 23:15:33 +0530

    Swim class

---
+++ Duck/Swim.java
@@ -0,0 +1,7 @@
+// Swim class implementing SwimBehavior
+public class Swim implements SwimBehavior {
+    @Override
+    public void swim() {
+        System.out.println("This duck can swim!");
+    }
+}
