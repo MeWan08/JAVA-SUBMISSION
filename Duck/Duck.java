1ec8c62cdab05d1446040ad933efa4327bcebf1c
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-08 23:11:13 +0530

    Non-Abstract functions

--- Duck/Duck.java
+++ Duck/Duck.java
@@ -2,4 +2,19 @@
 public abstract class Duck {
     FlyBehavior flyBehavior;
     SwimBehavior swimBehavior;
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performSwim() {
+        swimBehavior.swim();
+    }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setSwimBehavior(SwimBehavior sb) {
+        swimBehavior = sb;
+    }
 }
