4edcdfa85340eeba221c821d9bdf66623327e479
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-08 23:11:38 +0530

    Float class

---
+++ Duck/Float.java
@@ -0,0 +1,7 @@
+// Float class implementing SwimBehavior
+public class Float implements SwimBehavior {
+    @Override
+    public void swim() {
+        System.out.println("This duck can float on water.");
+    }
+}
