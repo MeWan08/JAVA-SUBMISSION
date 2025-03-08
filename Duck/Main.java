4a3f2ca9e527a68bc7dfcf9bb1e59d9adc6b97ff
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-08 23:13:03 +0530

    Function calling

--- Duck/Main.java
+++ Duck/Main.java
@@ -12,5 +12,26 @@ public class Main {
         Duck redheadDuck = new RedHeadDuck();
         Duck rubberDuck = new RubberDuck();
         Duck decoyDuck = new DecoyDuck();
+
+        // Display and perform behaviors
+        System.out.println("Mallard Duck:");
+        mallardDuck.display();
+        mallardDuck.performFly();
+        mallardDuck.performSwim();
+
+        System.out.println("\nRedhead Duck:");
+        redheadDuck.display();
+        redheadDuck.performFly();
+        redheadDuck.performSwim();
+
+        System.out.println("\nRubber Duck:");
+        rubberDuck.display();
+        rubberDuck.performFly();
+        rubberDuck.performSwim();
+
+        System.out.println("\nDecoy Duck:");
+        decoyDuck.display();
+        decoyDuck.performFly();
+        decoyDuck.performSwim();
     }
 }
