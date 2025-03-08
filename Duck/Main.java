20adee73f41b2329c8df470d889ffae2e9a52a05
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-03-08 23:12:53 +0530

    Object creation

--- Duck/Main.java
+++ Duck/Main.java
@@ -7,5 +7,10 @@ Batch: AIML A3
 // Main class to demonstrate Duck behaviors
 public class Main {
     public static void main(String[] args) {
+        // Create ducks
+        Duck mallardDuck = new MallardDuck();
+        Duck redheadDuck = new RedHeadDuck();
+        Duck rubberDuck = new RubberDuck();
+        Duck decoyDuck = new DecoyDuck();
     }
 }
