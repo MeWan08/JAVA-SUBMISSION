68d3bcb38ffb672d24b2adb0ff1857378605b00e
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-04-19 23:45:43 +0530

    Division function

--- Calculator/CalculatorOperations.java
+++ Calculator/CalculatorOperations.java
@@ -18,4 +18,12 @@ public class CalculatorOperations {
     public static double multiply(double a, double b) {
         return a * b;
     }
+
+    // Division
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
 }
