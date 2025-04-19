1e1530dc8bb51451953b0a8c653ca82e8a3d3e04
MeWan08 <157877376+MeWan08@users.noreply.github.com>
2025-04-19 23:53:22 +0530

    Class creation

---
+++ Threads/Main.java
@@ -0,0 +1,13 @@
+package Threads;
+
+/*
+Name: John Doe
+PRN: 1234567890
+Batch: 2023
+*/
+
+/**
+ * This class creates and starts two threads to demonstrate concurrent execution.
+ */
+public class Main {
+}
